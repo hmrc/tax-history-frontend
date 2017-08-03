ca6d6869fbf75e93464e50fed574f6ed219f4091
Kris Davison <kris.davison@digital.hmrc.gov.uk>
2017-08-03 16:17:09 +0100

    AFID-40 changed how we deal with authorised users who are not agents

--- test/controllers/MainControllerSpec.scala
+++ test/controllers/MainControllerSpec.scala
@@ -18,10 +18,10 @@ package controllers
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import config.{ConfigDecorator, FrontendAuthConnector}
+import config.{ConfigDecorator, FrontendAppConfig, FrontendAuthConnector}
 import connectors.TaxHistoryConnector
 import org.mockito.Matchers.{eq => meq, _}
-import org.mockito.Mockito.when
+import org.mockito.Mockito.{reset, when}
 import org.scalatest.mock.MockitoSugar
 import play.api.Application
 import play.api.http.Status
@@ -42,55 +42,74 @@ import scala.concurrent.Future
 
 class MainControllerSpec extends BaseSpec with MockitoSugar with Fixtures {
 
-  private val mockConnector = mock[TaxHistoryConnector]
-  private val mockFrontendAuthConnector = mock[FrontendAuthConnector]
-
   implicit val messagesApi = app.injector.instanceOf[MessagesApi]
   implicit val messages = messagesApi.preferred(FakeRequest())
 
   override implicit lazy val app: Application = new GuiceApplicationBuilder()
     .overrides(bind[FrontendAuthConnector].toInstance( mock[FrontendAuthConnector]))
     .overrides(bind[ConfigDecorator].toInstance(mock[ConfigDecorator]))
-    .overrides(bind[TaxHistoryConnector].toInstance(mockConnector))
+    .overrides(bind[TaxHistoryConnector].toInstance(mock[TaxHistoryConnector]))
     .build()
 
-
-  val validTestNINO = "AB123456B"
   val invalidTestNINO = "9999999999999999"
-
-
-  val validSelectClientForm = Seq(
-    "clientId" -> validTestNINO
-  )
 
   val invalidSelectClientForm = Seq(
     "clientId" -> invalidTestNINO
   )
 
+  lazy val authority = buildFakeAuthority(true)
+
+  lazy val newEnrolments = Set(
+    Enrolment("HMRC-AS-AGENT", Seq(EnrolmentIdentifier("AgentReferenceNumber", "TestArn")), confidenceLevel = ConfidenceLevel.L200,
+      state="",delegatedAuthRule = None)
+  )
+
+  lazy val fakeRequest = FakeRequest("GET", "/").withSession(
+    SessionKeys.sessionId -> "SessionId",
+    SessionKeys.token -> "Token",
+    SessionKeys.userId -> "/auth/oid/tuser",
+    SessionKeys.authToken -> ""
+  )
+
   trait LocalSetup {
-
-    lazy val authority = buildFakeAuthority(true)
-
-    val fakeRequest = FakeRequest("GET", "/").withSession(
-      SessionKeys.sessionId -> "SessionId",
-      SessionKeys.token -> "Token",
-      SessionKeys.userId -> "/auth/oid/tuser",
-      SessionKeys.authToken -> ""
-    )
 
     lazy val controller = {
       val employment = Employment(payeReference = "ABC", employerName = "Fred West Shoes", taxTotal = Some(BigDecimal.valueOf(123.12)), taxablePayTotal = Some(BigDecimal.valueOf(45.32)))
       val c = injected[MainController]
-      when(c.authConnector.authorise(any(), meq(EmptyRetrieval))(any())).thenReturn(Future.successful())
+      when(c.authConnector.authorise(any(), Matchers.any[Retrieval[~[Option[AffinityGroup], Enrolments]]]())(any())).thenReturn(
+        Future.successful(new ~[Option[AffinityGroup], Enrolments](Some(AffinityGroup.Agent) , Enrolments(newEnrolments))))
+      when(c.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.successful(HttpResponse(Status.OK,Some(Json.toJson(Seq(employment))))))
+      c
+    }
+  }
+
+  trait NotAgentSetup {
+
+    lazy val controller = {
+      val employment = Employment(payeReference = "ABC", employerName = "Fred West Shoes", taxTotal = Some(BigDecimal.valueOf(123.12)), taxablePayTotal = Some(BigDecimal.valueOf(45.32)))
+      val c = injected[MainController]
+      when(c.authConnector.authorise(any(), Matchers.any[Retrieval[~[Option[AffinityGroup], Enrolments]]]())(any())).thenReturn(
+        Future.successful(new ~[Option[AffinityGroup], Enrolments](Some(AffinityGroup.Individual) , Enrolments(newEnrolments))))
+      when(c.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.successful(HttpResponse(Status.OK,Some(Json.toJson(Seq(employment))))))
+      c
+    }
+  }
+
+  trait NoEnrolmentsSetup {
+
+    lazy val controller = {
+      val employment = Employment(payeReference = "ABC", employerName = "Fred West Shoes", taxTotal = Some(BigDecimal.valueOf(123.12)), taxablePayTotal = Some(BigDecimal.valueOf(45.32)))
+      val c = injected[MainController]
+      when(c.authConnector.authorise(any(), Matchers.any[Retrieval[~[Option[AffinityGroup], Enrolments]]]())(any())).thenReturn(
+        Future.successful(new ~[Option[AffinityGroup], Enrolments](Some(AffinityGroup.Agent) , Enrolments(Set()))))
       when(c.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.successful(HttpResponse(Status.OK,Some(Json.toJson(Seq(employment))))))
       c
     }
   }
 
   "GET /tax-history" should {
-
     "return 200" in new LocalSetup {
-      val result = controller.get()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      val result = controller.getTaxHistory().apply(FakeRequest().withSession("USER_NINO" -> "AA000003A"))
       status(result) shouldBe Status.OK
     }
 
@@ -98,7 +117,7 @@ class MainControllerSpec extends BaseSpec with MockitoSugar with Fixtures {
       implicit val actorSystem = ActorSystem("test")
       implicit val materializer = ActorMaterializer()
       when(controller.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.successful(HttpResponse(Status.NOT_FOUND,Some(Json.toJson("[]")))))
-      val result = controller.get()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      val result = controller.getTaxHistory()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
       status(result) shouldBe Status.OK
       bodyOf(await(result)) should include(Messages("employmenthistory.notfound.header", "AA000003A").toString)
     }
@@ -107,7 +126,7 @@ class MainControllerSpec extends BaseSpec with MockitoSugar with Fixtures {
       implicit val actorSystem = ActorSystem("test")
       implicit val materializer = ActorMaterializer()
       when(controller.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.successful(HttpResponse(Status.UNAUTHORIZED,Some(Json.toJson("{Message:Unauthorised}")))))
-      val result = controller.get()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      val result = controller.getTaxHistory()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
       status(result) shouldBe Status.OK
       bodyOf(await(result)) should include(Messages("employmenthistory.unauthorised.header", "AA000003A"))
     }
@@ -116,7 +135,7 @@ class MainControllerSpec extends BaseSpec with MockitoSugar with Fixtures {
       implicit val actorSystem = ActorSystem("test")
       implicit val materializer = ActorMaterializer()
       when(controller.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.successful(HttpResponse(Status.INTERNAL_SERVER_ERROR,Some(Json.toJson("{Message:InternalServerError}")))))
-      val result = controller.get()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      val result = controller.getTaxHistory()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
       status(result) shouldBe Status.OK
       bodyOf(await(result)) should include(Messages("employmenthistory.technicalerror.header"))
     }
@@ -124,8 +143,7 @@ class MainControllerSpec extends BaseSpec with MockitoSugar with Fixtures {
     "show technical error page when no nino has been set in session" in new LocalSetup {
       implicit val actorSystem = ActorSystem("test")
       implicit val materializer = ActorMaterializer()
-      //when(controller.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.successful(HttpResponse(Status.INTERNAL_SERVER_ERROR,Some(Json.toJson("{Message:InternalServerError}")))))
-      val result = controller.get()(fakeRequest)
+      val result = controller.getTaxHistory()(fakeRequest)
       status(result) shouldBe Status.OK
       bodyOf(await(result)) should include(Messages("mtdfi.select_client.title"))
     }
@@ -134,22 +152,34 @@ class MainControllerSpec extends BaseSpec with MockitoSugar with Fixtures {
       implicit val actorSystem = ActorSystem("test")
       implicit val materializer = ActorMaterializer()
       when(controller.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.failed(new BadGatewayException("")))
-      val result = controller.get()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      val result = controller.getTaxHistory()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
       status(result) shouldBe Status.OK
       bodyOf(await(result)) should include(Messages("employmenthistory.technicalerror.header"))
     }
 
     "redirect to gg when not logged in" in new LocalSetup {
       when(controller.taxHistoryConnector.getTaxHistory(any(), any())(any())).thenReturn(Future.failed(new MissingBearerToken))
-      val result = controller.get()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      val result = controller.getTaxHistory()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
       status(result) shouldBe Status.SEE_OTHER
+      await(result.header.headers.get("Location")).get should include("/gg/sign-in")
     }
 
     "return Status: 400 when invalid data is input" in new LocalSetup {
       val result = controller.submitSelectClientPage().apply(FakeRequest()
         .withFormUrlEncodedBody(invalidSelectClientForm: _*))
-
       status(result) shouldBe Status.BAD_REQUEST
+    }
+
+    "redirect to when agent has no enrolments" in new NoEnrolmentsSetup {
+      val result = controller.getTaxHistory()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      status(result) shouldBe Status.SEE_OTHER
+      await(result.header.headers.get("Location")) shouldBe Some(FrontendAppConfig.AfiNoAgentServicesAccountPage)
+    }
+
+    "redirect when user is not an agent" in new NotAgentSetup {
+      val result = controller.getTaxHistory()(fakeRequest.withSession("USER_NINO" -> "AA000003A"))
+      status(result) shouldBe Status.SEE_OTHER
+      await(result.header.headers.get("Location")) shouldBe Some(FrontendAppConfig.AfiNoAgentServicesAccountPage)
     }
   }
 
