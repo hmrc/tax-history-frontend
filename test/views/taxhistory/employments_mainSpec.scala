956232af3d0663790e2e13a6c0b652e88edbd471
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-08-31 15:56:13 +0100

    update UI with eyu employment data

--- test/views/taxhistory/employments_mainSpec.scala
+++ test/views/taxhistory/employments_mainSpec.scala
@@ -21,9 +21,10 @@ import org.joda.time.LocalDate
 import org.scalatest.MustMatchers
 import play.api.i18n.Messages
 import uk.gov.hmrc.urls.Link
+import utils.DateHelper
 import views.{Fixture, GenericTestHelper}
 
-class employments_mainSpec extends GenericTestHelper with MustMatchers {
+class employments_mainSpec extends GenericTestHelper with MustMatchers with EmpConstants {
 
   trait ViewFixture extends Fixture {
     implicit val requestWithToken = addToken(request)
@@ -35,16 +36,9 @@ class employments_mainSpec extends GenericTestHelper with MustMatchers {
   }
 
   "employments_main view" must {
-    val employments = List(Employment("AA12341234", "Test Employer Name", Some(25000.0), Some(2000.0), List.empty,
-      List(CompanyBenefit("Benifit1", 1000.00), CompanyBenefit("Benifit2", 2000.00))),
-      Employment("AA111111", "Test Employer Name", Some(25000.0), Some(2000.0), List.empty,
-        List(CompanyBenefit("Benifit2", 2000.00), CompanyBenefit("Benifit2", 2000.00))))
-    val allowance = List(Allowance("desc", 222.00),Allowance("desc1", 333.00))
-    val paye = PayAsYouEarnDetails(employments, allowance)
-
 
     "have correct title and heading" in new ViewFixture {
-      val view = views.html.taxhistory.employments_main( nino, taxYear, paye, None)
+      val view = views.html.taxhistory.employments_main( nino, taxYear, payePartialModel, None)
 
       val title = Messages("employmenthistory.title")
       heading.html must be(Messages("employmenthistory.header",nino))
@@ -55,29 +49,29 @@ class employments_mainSpec extends GenericTestHelper with MustMatchers {
 
     "include employment breakdown" in new ViewFixture {
 
-      val employments = Seq(Employment("AA12341234", "Test Employer Name", Some(25000.0), Some(2000.0),
-        List(EarlierYearUpdate(taxablePayEYU = BigDecimal.valueOf(-21.00), taxEYU = BigDecimal.valueOf(-4.56), LocalDate.parse("2017-08-30")))))
-      val view = views.html.taxhistory.employments_main(nino, taxYear, paye, None)
+      val view = views.html.taxhistory.employments_main(nino, taxYear, payeCompleteModel, None)
 
       val tableRowPay = doc.select(".employment-table tbody tr").get(2)
 
       tableRowPay.text must include(employments.head.taxablePayTotal.get.toString())
       doc.getElementsMatchingOwnText(Messages("lbl.company.benefits")).hasText mustBe true
       doc.getElementsMatchingOwnText(Messages("employmenthistory.allowance.heading")).hasText mustBe true
-      doc.getElementsMatchingOwnText(allowance.head.typeDescription).hasText mustBe true
-      doc.getElementsMatchingOwnText(allowance.head.amount.toString()).hasText mustBe true
-      doc.getElementsMatchingOwnText(allowance.last.typeDescription).hasText mustBe true
-      doc.getElementsMatchingOwnText(allowance.last.amount.toString()).hasText mustBe true
+      doc.getElementsMatchingOwnText(allowances.head.amount.toString()).hasText mustBe true
+      doc.getElementsMatchingOwnText(allowances.last.typeDescription).hasText mustBe true
+      doc.getElementsMatchingOwnText(allowances.last.amount.toString()).hasText mustBe true
+
+      doc.getElementsMatchingOwnText(Messages("employmenthistory.eyu.pay")).hasText mustBe true
+      doc.getElementsMatchingOwnText(Messages("employmenthistory.eyu.tax")).hasText mustBe true
+      doc.getElementsMatchingOwnText(Messages("employmenthistory.eyu.date.received")).hasText mustBe true
+      doc.getElementsMatchingOwnText(employments.head.earlierYearUpdates.head.taxablePayEYU.toString()).hasText mustBe true
+      doc.getElementsMatchingOwnText(employments.head.earlierYearUpdates.head.taxEYU.toString()).hasText mustBe true
+      doc.getElementsMatchingOwnText(DateHelper.formatDate(employments.head.earlierYearUpdates.head.receivedDate)).hasText mustBe true
 
     }
 
     "allow partial employment top be displayed" in new ViewFixture {
 
-
-      val employments = List(Employment("AA12341234", "Test Employer Name", None, None))
-      val paye1 = PayAsYouEarnDetails(employments, List.empty)
-
-      val view = views.html.taxhistory.employments_main(nino, taxYear, paye1, None)
+      val view = views.html.taxhistory.employments_main(nino, taxYear, payePartialModel, None)
 
       val tableRowPay = doc.select(".employment-table tbody tr").get(2)
       tableRowPay.text must include(messagesApi("employmenthistory.nopaydata"))
@@ -91,16 +85,17 @@ class employments_mainSpec extends GenericTestHelper with MustMatchers {
       val link = Link.toExternalPage(id=Some("sidebarLink"), url="http://www.google.com", value=Some("Back To Google")).toHtml
       val employments = Seq()
 
-      val view = views.html.taxhistory.employments_main(nino, taxYear, paye, None, Some(link))
+      val view = views.html.taxhistory.employments_main(nino, taxYear, payePartialModel, None, Some(link))
 
       val sideBarLinks = doc.select("#sidebarLink")
       sideBarLinks.size mustBe 1
       val sideBarLink = sideBarLinks.get(0)
       sideBarLink.text must include("Back To Google")
     }
+
     "include persons first and last name" in new ViewFixture {
       val employments = Seq()
-      val view = views.html.taxhistory.employments_main(nino, taxYear, paye, person)
+      val view = views.html.taxhistory.employments_main(nino, taxYear, payePartialModel, person)
       val names = doc.select("#name")
       names.size mustBe 1
       val name = names.get(0)
@@ -108,5 +103,20 @@ class employments_mainSpec extends GenericTestHelper with MustMatchers {
 
     }
   }
+}
 
+trait EmpConstants {
+  val companyBenefit1 = CompanyBenefit("Benefit1", 1000.00)
+  val companyBenefit2 = CompanyBenefit("Benefit2", 2000.00)
+  val EarlierYearUpdateList = List(EarlierYearUpdate(BigDecimal.valueOf(-21.00), BigDecimal.valueOf(-4.56), LocalDate.parse("2017-08-30")))
+  val emp1 =  Employment("AA12341234", "Test Employer Name", Some(25000.0), Some(2000.0), EarlierYearUpdateList,
+    List(companyBenefit1, companyBenefit2))
+  val emp2 = Employment("AA111111", "Test Employer Name", Some(25000.0), Some(2000.0), List.empty,
+    List(companyBenefit1, companyBenefit2))
+  val employments = List(emp1,emp2)
+  val allowances = List(Allowance("desc", 222.00),Allowance("desc1", 333.00))
+  val payeCompleteModel = PayAsYouEarnDetails(employments, allowances)
+
+  val partialEmploymentList = List(Employment("AA12341234", "Test Employer Name", None, None))
+  val payePartialModel = PayAsYouEarnDetails(partialEmploymentList, List.empty)
 }
