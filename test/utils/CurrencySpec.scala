d97d55e23c33cc10ba3fb5585cc318de003b7641
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-09-14 10:11:32 +0100

    update test

--- test/utils/CurrencySpec.scala
+++ test/utils/CurrencySpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package utils
 
 import org.scalatestplus.play.PlaySpec
@@ -6,11 +22,11 @@ class CurrencySpec extends PlaySpec {
 
   "Currency" must {
     "format +ve value successfully" in {
-      Currency(BigDecimal(1000.55)).toString must be("£1,000.55")
+      Currency(BigDecimal(100.55)).toString must be("£100.55")
     }
 
     "format -ve value successfully" in {
-      Currency(BigDecimal(-1000.55)).toString must be("-£1000.55")
+      Currency(BigDecimal(-100.55)).toString must be("-£100.55")
     }
   }
 }
