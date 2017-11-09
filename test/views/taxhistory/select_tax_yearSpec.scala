47b07ceb2aa42d1f8aeb6b4a79e02a529a6717c4
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-11-09 12:12:27 +0000

    AFID-215:add new UI for select tax year and related test

---
+++ test/views/taxhistory/select_tax_yearSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.taxhistory
+
+import play.api.i18n.Messages
+import support.GuiceAppSpec
+import views.Fixture
+
+class select_tax_yearSpec extends GuiceAppSpec {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+  }
+
+  "select_tax_year view" must {
+
+    "have correct title, heading and GA pageview event" in new ViewFixture {
+
+      val view = views.html.taxhistory.select_tax_year("Name")
+
+      doc.title mustBe Messages("employmenthistory.select.tax.year.title")
+      doc.select("h1").text() mustBe Messages("employmenthistory.select.tax.year.header", "Name")
+      doc.select("script").toString contains
+        "ga('send', 'pageview', { 'anonymizeIp': true })" mustBe true
+    }
+  }
+
+}
+
