46ebd16e4a4f9220cdd28ff9cfdc0e9d9db9af7e
AdamShahzad <adam.shahzad@outlook.com>
2018-03-21 11:21:48 +0000

    [AFID-459] reformated if statement in currency to be more readable

--- app/utils/Currency.scala
+++ app/utils/Currency.scala
@@ -23,17 +23,11 @@ case class Currency(value: BigDecimal, minDecimalPlaces: Int = 0) {
   override def toString: String = {
     val formatter = java.text.NumberFormat.getCurrencyInstance(Locale.UK)
 
-    if (value % 1 != 0){
-      formatter.setMinimumFractionDigits(2)
-    }else{
-      formatter.setMinimumFractionDigits(minDecimalPlaces)
-    }
+    if (value % 1 != 0)formatter.setMinimumFractionDigits(2)
+    else formatter.setMinimumFractionDigits(minDecimalPlaces)
 
-    if (value.signum >= 0) {
-      formatter.format(value)
-    }else{
-      "- " + formatter.format(value.abs)
-    }
+    if (value.signum >= 0)formatter.format(value)
+    else "- " + formatter.format(value.abs)
   }
 
 }
