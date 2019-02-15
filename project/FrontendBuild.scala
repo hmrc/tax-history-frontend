060741de09aecad95e9814f8ec0858cefae74c90
Raveen Vijayan <raveen.vijayan@digital.hmrc.gov.uk>
2019-02-15 14:52:57 +0000

    [ASA-420][RV] update dependencies in frontend build file

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -10,24 +10,24 @@ object FrontendBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "domain" % "5.2.0",
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.7.0",
+    "uk.gov.hmrc" %% "domain" % "5.3.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.9.0",
     "uk.gov.hmrc" %% "govuk-template" % "5.22.0",
-    "uk.gov.hmrc" %% "play-ui" % "7.18.0",
-    "uk.gov.hmrc" %% "play-partials" % "6.1.0",
-    "uk.gov.hmrc" %% "url-builder" % "2.1.0",
-    "uk.gov.hmrc" %% "auth-client" % "2.6.0",
-    "uk.gov.hmrc" %% "agent-mtd-identifiers" % "0.10.0",
+    "uk.gov.hmrc" %% "play-ui" % "7.32.0-play-25",
+    "uk.gov.hmrc" %% "play-partials" % "6.5.0",
+    "uk.gov.hmrc" %% "url-builder" % "3.1.0",
+    "uk.gov.hmrc" %% "auth-client" % "2.19.0-play-25",
+    "uk.gov.hmrc" %% "agent-mtd-identifiers" % "0.13.0",
     "uk.gov.hmrc" %% "tax-year" % "0.4.0"
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.4.0-play-25" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.10.2" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.mockito" % "mockito-all" % "2.0.2-beta" % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope
   )
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -14,5 +14,3 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.2.0")
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.19")
 
 addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.12")
-
-addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.5.2")
