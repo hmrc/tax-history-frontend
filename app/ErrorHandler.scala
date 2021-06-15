02ae810de674d4d948b3f011bd6608f447030245
James-Rourke <70650004+James-Rourke@users.noreply.github.com>
2021-06-15 16:34:30 +0100

    Tax-history-frontend Bootstrap Upgrade for Auth Client v5.6.0 (#282)
    Tax-history-frontend Bootstrap Upgrade for Auth Client v5.6.0

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -16,17 +16,18 @@
 
 import com.google.inject.name.Named
 import config.AppConfig
-import javax.inject.{Inject, Singleton}
+import play.api.http.Status._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Request, RequestHeader}
 import play.api.{Configuration, Environment}
 import play.twirl.api.Html
-import uk.gov.hmrc.http.{JsValidationException, NotFoundException}
+import uk.gov.hmrc.http.{JsValidationException, Upstream4xxResponse}
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.bootstrap.config.{AuthRedirects, HttpAuditEvent}
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
 
+import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
 @Singleton
@@ -69,12 +70,12 @@ trait ErrorAuditing extends HttpAuditEvent {
 
   def auditServerError(request: RequestHeader, ex: Throwable)(implicit ec: ExecutionContext): Unit = {
     val eventType = ex match {
-      case _: NotFoundException     => ResourceNotFound
+      case Upstream4xxResponse(_, NOT_FOUND, _, _)   => ResourceNotFound
       case _: JsValidationException => ServerValidationError
       case _                        => ServerInternalError
     }
     val transactionName = ex match {
-      case _: NotFoundException => notFoundError
+      case Upstream4xxResponse(_, NOT_FOUND, _, _) => notFoundError
       case _                    => unexpectedError
     }
     auditConnector.sendEvent(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,9 +2,7 @@ resolvers += "HMRC-open-artefacts-maven" at "https://open.artefacts.tax.service.
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "2.15.0")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.2.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.0.0")
 
 addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "1.6.1")
 
