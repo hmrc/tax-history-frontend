86a1b45b646f75fe33f875c63033e0317a9911c8
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2022-10-31 14:30:44 +0000

    [DDCE-3010] Tab Name Change (#319)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 
 object AppDependencies {
 
-  val silencerVersion = "1.7.9"
+  val silencerVersion = "1.7.12"
 
   private val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "domain"                     % "8.1.0-play-28",
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.2.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.24.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.8.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.32.0-play-28",
     "uk.gov.hmrc" %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc" %% "url-builder"                % "3.6.0-play-28",
     "uk.gov.hmrc" %% "agent-mtd-identifiers"      % "0.47.0-play-28",
@@ -17,7 +17,7 @@ object AppDependencies {
   )
 
   private val test: Seq[ModuleID]                 = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.2.13",
+    "org.scalatest"          %% "scalatest"          % "3.2.14",
     "org.jsoup"               % "jsoup"              % "1.15.3",
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current,
     "org.mockito"            %% "mockito-scala"      % "1.17.12",
