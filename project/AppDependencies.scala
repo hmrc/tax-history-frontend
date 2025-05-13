5057a7fe006e64f72ebb8e305a3c5c0e667aa02f
lmugnai <11977337+lmugnai@users.noreply.github.com>
2025-05-13 16:17:22 +0100

    DDCE-6660: Update agent client relationship fast track url

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,14 +6,14 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.0.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.1.0",
     "uk.gov.hmrc" %% "domain-play-30"             % "11.0.0",
     "uk.gov.hmrc" %% "tax-year"                   % "6.0.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion,
-    "org.jsoup"    % "jsoup"                  % "1.19.1"
+    "org.jsoup"    % "jsoup"                  % "1.20.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
