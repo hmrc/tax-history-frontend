b876e45fcd755658a8a1effcfd788b833092546e
SachaOkus <104149202+SachaOkus@users.noreply.github.com>
2025-09-02 18:28:47 +0100

    DDCE-6865: Upgrade play-frontend-hmrc to 12.10.0 and add frontend.js for rebrand

---
+++ app/assets/javascripts/frontend.js
@@ -0,0 +1,3 @@
+//= require frontend_all
+import { initAll } from 'govuk-frontend'
+document.addEventListener('DOMContentLoaded', () => initAll())
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.1.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.10.0",
     "uk.gov.hmrc" %% "domain-play-30"             % "11.0.0",
     "uk.gov.hmrc" %% "tax-year"                   % "6.0.0"
   )
