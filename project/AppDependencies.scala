7bc9d37d88e323791d6b89521b4897ff4be58a5e
Sacha <104149202+SachaOkus@users.noreply.github.com>
2025-09-17 14:11:41 +0100

    DDCE-6865: Upgrade play-frontend-hmrc to 12.10.0 and add frontend.js for rebrand (#379)
    * DDCE-6865: Upgrade play-frontend-hmrc to 12.10.0 and add frontend.js for rebrand
    
    * Header alignment fixed, dependancies updated
    
    * dependency update
    
    * compile
    
    * govukHeader: GovukHeader line removed
    
    * import uk.gov.hmrc.govukfrontend.views.viewmodels.header._ removed
    
    * test: signed commit
    
    * This reverts commit f006e05a58b5df52b311f8b857e205d03a937111.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,8 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.11.0",
+
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.12.0",
     "uk.gov.hmrc" %% "domain-play-30"             % "11.0.0",
     "uk.gov.hmrc" %% "tax-year"                   % "6.0.0"
   )
